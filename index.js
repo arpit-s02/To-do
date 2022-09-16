ce25aa25e57a645c8268b2b0b18daef64e3a023c
Arpit <arpitrahul02@gmail.com>
2022-09-16 13:03:06 +0530

    create directory structure and setup express server

---
+++ index.js
@@ -0,0 +1,12 @@
+// require express to use it
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.listen(port, (err) => {
+    if(err){
+        console.log(`Error in stating express: ${err}`);
+        return;
+    }
+    console.log(`Express server is up and running on port ${port}`);
+});
