419d90e14e65bb70fb939b98e27fe9c207f0df26
Arpit <arpitrahul02@gmail.com>
2022-09-21 01:30:04 +0530

    Finalize the Project

--- controllers/delete_controller.js
+++ controllers/delete_controller.js
@@ -3,15 +3,6 @@ const Task = require('../models/task');
 module.exports.delete = async (req, res) => {
     var items = req.query.item;
     if(Array.isArray(items)){
-        // for(let i in items){
-        //     let id = items[i];
-        //     Task.findByIdAndDelete(id, (err) => {
-        //         if(err){
-        //             console.log('Error in deleting tasks');
-        //             return;
-        //         }
-        //     });
-        // }
         await Promise.all(
             items.map(async (item) => {
                 await Task.findByIdAndDelete(item);
